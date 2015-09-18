220644302b72ed20c622f5582031b797a608d093
Joelle Dyer <jmdyer@uvm.edu>
2015-09-18 01:36:39 -0400

    changes

--- assignment2.0/q03.php
+++ assignment2.0/q03.php
@@ -1,8 +1,1 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!-- SELECT * FROM tblSections WHERE fldStart = '13:10:00' -->
