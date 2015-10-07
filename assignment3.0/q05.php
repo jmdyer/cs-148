41c8886ac1115a375af67bff1f97763357950f2d
Joelle Dyer <jmdyer@uvm.edu>
2015-10-06 20:13:08 -0400

    updated q05

--- assignment3.0/q05.php
+++ assignment3.0/q05.php
@@ -6,13 +6,15 @@ include "top.php";
 <?php
 print '<body id="' . $path_parts['filename'] . '">';
 //now print out each record
-$query = 'SELECT tblTeachers.fldFirstName, tblTeachers.fldLastName, COUNT(fnkStudentId) AS "total" FROM tblTeachers, tblSections, tblEnrolls
-WHERE tblTeachers.pmkNetId = tblSections.fnkTeacherNetId
-AND tblSections.fldCRN = tblEnrolls.fnkSectionId
-AND fldType <> "LAB"
-GROUP BY tblTeachers.fldLastName, tblTeachers.fldFirstName
-ORDER BY `total`  DESC';
-$info2 = $thisDatabaseReader->select($query, "", 0, 0, 4, 2, false, false);
+$query = 'SELECT tblTeachers.fldFirstName, tblTeachers.fldLastName,  count(tblStudents.fldFirstName) as total
+FROM tblSections
+JOIN tblEnrolls on tblSections.fldCRN  = tblEnrolls.`fnkSectionId`
+JOIN tblStudents on pmkStudentId = fnkStudentId
+JOIN tblTeachers on tblSections.fnkTeacherNetId=pmkNetId
+WHERE fldType != "LAB"
+group by fnkTeacherNetId
+ORDER BY total desc';
+$info2 = $thisDatabaseReader->select($query, "", 0, 1, 2, 0, false, false);
 //$test = $thisDatabaseReader->testquery($query, $data, 0, 0, 0, 0, false, false); 
 $columns = 3;
 print '<h2>Total Records: ' . count($info2) . '</h2>';
